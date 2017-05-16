2163a185b31d2877174211f9c68f2ab5a54b5e1a
Bridger <bridgerrhammond@gmail.com>
2017-05-16 17:14:17 -0400

    So many charts. Only need 3 more!

--- js/localCharts.js
+++ js/localCharts.js
@@ -1,122 +1,188 @@
 // Colors
-const colorBase = '#009688';
+var colorBase = '#009688';
 // Shades
-const color100 = '#A7FFEB';
-const color200 = '#64FFDA';
-const color400 = '#1DE9B6';
-const color700 = '#00BFA5';
+var color100 = '#A7FFEB';
+var color200 = '#64FFDA';
+var color400 = '#1DE9B6';
+var color700 = '#00BFA5';
 
 
 // Traffic Charts
-Chart.defaults.global.legend.display = false;
+// Chart.defaults.global.legend.display = false;
+Chart.defaults.global.legend.responsive = true;
 
 // Chart constants
 const trafficChartHourly = document.getElementById('traffic-hourly');
 const trafficChartDaily = document.getElementById('traffic-daily');
 const trafficChartWeekly = document.getElementById('traffic-weekly');
 const trafficChartMonthly = document.getElementById('traffic-monthly');
-
+// Stand-Alone Chart Constants
 const dailyStandAlone = document.getElementById('trafficDaily');
+const mobileChart = document.getElementById('mobileChart');
+const twitterChart = document.getElementById('twitterChart');
+const facebookChart = document.getElementById('facebookChart');
+const googlePlusChart = document.getElementById('googlePlusChart');
 
 // Hourly Traffic
 let trafficHourly = new Chart.Line(trafficChartHourly, {
-data: {
-  labels: ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  datasets: [
-        {
-          label: '',
-          data: [5, 8, 10, 45, 7, 15, 25, 60, 43, 22, 31],  
-          lineTension: 0,
-          borderWidth: 3,
-          pointBorderWidth: 3,
-          pointRadius: 4,
-          pointHoverRadius: 8,
-          pointStyle: 'circle',
-          showLine: true,
-          spanGaps: true,          
-        }
-    ]
-},
+  options: {
+    legend: {
+      display: false,
+    },
+  },
+  data: {
+    labels: ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    datasets: [
+          {
+            label: '',
+            data: [5, 8, 10, 45, 7, 15, 25, 60, 43, 22, 31],  
+            lineTension: 0,
+            borderWidth: 3,
+            pointBorderWidth: 3,
+            pointRadius: 4,
+            pointHoverRadius: 8,
+            pointStyle: 'circle',
+            showLine: true,
+            spanGaps: true,          
+          }
+      ]
+  },
 });
 
 // Daily Traffic
 let trafficDaily = new Chart.Line(trafficChartDaily, {
-data: {
-  labels: ['S', 'M', 'T', 'W', 'TH', 'F', 'S'],
-  datasets: [
-        {
-          label: '',
-          data: [175, 50, 78, 105, 80, 225, 187],  
-          lineTension: 0,
-          pointBorderWidth: 3,
-          pointRadius: 4,
-          pointHoverRadius: 8,
-          pointStyle: 'circle',
-          showLine: true,
-          spanGaps: true,          
-        }
-    ]
-},
+  options: {
+    legend: {
+      display: false,
+    },
+  },
+  data: {
+    labels: ['S', 'M', 'T', 'W', 'TH', 'F', 'S'],
+    datasets: [
+          {
+            label: '',
+            data: [175, 50, 78, 105, 80, 225, 187],  
+            lineTension: 0,
+            pointBorderWidth: 3,
+            pointRadius: 4,
+            pointHoverRadius: 8,
+            pointStyle: 'circle',
+            showLine: true,
+            spanGaps: true,          
+          }
+      ]
+  },
 });
 
 //Weekly Traffic
 let trafficWeekly = new Chart.Line(trafficChartWeekly, {
-data: {
-  labels: ['16-22', '23-29', '30-6', '7-13', '14-20', '21-27', '28-3', '4-10', '11-17'],
-  datasets: [
-        {
-          label: '',
-          data: [400, 600, 750, 625, 725, 850, 950, 700, 725],  
-          lineTension: 0,
-          pointBorderWidth: 3,
-          pointRadius: 4,
-          pointHoverRadius: 8,
-          pointStyle: 'circle',
-          showLine: true,
-          spanGaps: true,          
-        }
-    ]
-},
+  options: {
+    legend: {
+      display: false,
+    },
+  },
+  data: {
+    labels: ['16-22', '23-29', '30-6', '7-13', '14-20', '21-27', '28-3', '4-10', '11-17'],
+    datasets: [
+          {
+            label: '',
+            data: [400, 600, 750, 625, 725, 850, 950, 700, 725],  
+            lineTension: 0,
+            pointBorderWidth: 3,
+            pointRadius: 4,
+            pointHoverRadius: 8,
+            pointStyle: 'circle',
+            showLine: true,
+            spanGaps: true,          
+          }
+      ]
+  },
 }); 
 
 // Monthly Traffic
 let trafficMonthly = new Chart.Line(trafficChartMonthly, {
-data: {
-  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  datasets: [
-        {
-          label: '',
-          data: [3680, 2530, 1950, 5654, 4250, 6507, 2489, 6865, 7800, 5010, 8426, 3689],
-          lineTension: 0,
-          pointBorderWidth: 3,
-          pointRadius: 4,
-          pointHoverRadius: 8,
-          pointStyle: 'circle',
-          showLine: true,
-          spanGaps: true,          
-        }
-    ]
-},
+  options: {
+    legend: {
+      display: false,
+    },
+  },
+  data: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    datasets: [
+          {
+            label: '',
+            data: [3680, 2530, 1950, 5654, 4250, 6507, 2489, 6865, 7800, 5010, 8426, 3689],
+            lineTension: 0,
+            pointBorderWidth: 3,
+            pointRadius: 4,
+            pointHoverRadius: 8,
+            pointStyle: 'circle',
+            showLine: true,
+            spanGaps: true,          
+          }
+      ]
+  },
 }); 
 
 // Stand-Alone Charts
 
 // Daily
-let dailyChart = new Chart.Line(dailyStandAlone, {
-data: {
-  labels: ['S', 'M', 'T', 'W', 'TH', 'F', 'S'],
-  datasets: [
-        {
-          label: '',
-          data: [175, 50, 78, 105, 80, 225, 187],  
-          lineTension: 0,
-          pointBorderWidth: 3,
-          pointRadius: 4,
-          pointHoverRadius: 8,
-          pointStyle: 'circle',
-          showLine: true,
-          spanGaps: true,          
-        }
-    ]
-},
+const dailyChart = new Chart.Line(dailyStandAlone, {
+  options: {
+    legend: {
+      display: false,
+    },
+  },
+  data: {
+    labels: ['S', 'M', 'T', 'W', 'TH', 'F', 'S'],
+    datasets: [
+          {
+            label: '',
+            data: [175, 50, 78, 105, 80, 225, 187],  
+            lineTension: 0,
+            pointBorderWidth: 3,
+            pointRadius: 4,
+            pointHoverRadius: 8,
+            pointStyle: 'circle',
+            showLine: true,
+            spanGaps: true,          
+          }
+      ]
+  },
 });
+
+// Mobile Users
+let mobileUsersChart = new Chart(mobileChart, {
+  options: {
+    legend: {
+      position: 'right',
+      display: true,
+    },
+  },
+  type: 'doughnut',
+  data: {
+  labels: [
+        "Phones",
+        "Tablets",
+        "Desktop"
+    ],
+    datasets: [
+        {
+            data: [65, 20, 15],
+            backgroundColor: [
+                color700,
+                color400,
+                color200
+            ],
+            hoverBackgroundColor: [
+                color100,
+                color100,
+                color100
+            ]
+        }]
+  }
+});
+
+// Twitter
+// Facebook
+// Google+
