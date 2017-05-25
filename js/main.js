e1492647097428910db3a6717ddbf6fa1dd3d8a2
Bridger <bridgerrhammond@gmail.com>
2017-05-25 13:26:23 -0400

    Working on alerts javascript

--- js/main.js
+++ js/main.js
@@ -1,21 +1,51 @@
-// Tab Script
+// Alerts
+function displayAlerts() {
+    document.getElementById('notificationOne').style.display='inline-flex';
+    document.getElementById('notificationTwo').style.display='inline-flex';
+    document.getElementById('dashboardHeader').style.marginBottom = '+70px';
+}
+
+function closeAlert1() {
+    document.getElementById('notificationOne').style.display='none';
+    document.getElementById('notificationOne').innerHTML='';
+    document.getElementById('dashboardHeader').style.marginBottom =  ; // Figure out how to subtract X from margin
+}
+function closeAlert2() {
+    document.getElementById('notificationTwo').style.display='none';
+    document.getElementById('notificationTwo').innerHTML='';
+    document.getElementById('dashboardHeader').style.marginBottom = '35px';
+}
+
+// Chart Script
 function openChart(evt, chartTime) {
     // Declare all variables
     var i, tabcontent, tablinks;
-
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(chartTime).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+// Message popups
+function validateMessage() {
+    var x = document.getElementById('userSearchBox').value;
+    var y = document.getElementById('userMessageBox').value;
+    if (x === '') {
+        alert("Please specify a user to message.");
+    } else if (y === '') {
+        alert("Please enter a message.");
+    } else {
+        window.alert("MESSAGE SENT!");
+        document.getElementById('userSearchBox').value = '';
+        document.getElementById('userMessageBox').value = '';
+    }
 }
